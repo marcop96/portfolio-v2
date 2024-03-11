d9586807c533920d01bdc4c25e035baac46e525e
“Marco <marco.pianaroli96@gmail.com>
2024-03-11 21:35:24 +0100

    feat: Projects component

---
+++ src/components/TheProjects.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import { projects } from "../consts/Projects";
+</script>
+
+<template>
+  <li v-for="project in projects" :key="project.title">
+    <h2>{{ project.title }}</h2>
+    <p>{{ project.description }}</p>
+  </li>
+</template>
