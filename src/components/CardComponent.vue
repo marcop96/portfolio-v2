955d6510ef7476324d5cc1efa1ca81cd325fefb1
“Marco <marco.pianaroli96@gmail.com>
2024-03-11 21:51:06 +0100

    feat:creates component and recieves props

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Project } from "../../types";
+
+const props = defineProps<{
+  project: Project;
+}>();
+const { title, description, image, technologies } = props.project;
+
+console.log(title, description, image, technologies);
+</script>
+
+<template>
+  <main class="container">
+    {{ title }}
+    {{ description }}
+    {{ image }}
+    {{ technologies }}
+  </main>
+</template>
