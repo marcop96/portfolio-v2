a042ea1826c89ad0a793cb561b09245e3e8ad844
“Marco <marco.pianaroli96@gmail.com>
2024-03-12 11:43:40 +0100

    feat: displays project technologies in icons

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,19 +1,43 @@
 <script setup lang="ts">
 import type { Project } from "../../types";
-
+import PillButton from "./PillButton.vue";
 const props = defineProps<{
   project: Project;
 }>();
-const { title, description, image, technologies } = props.project;
+const { title, description, image, technologies, links } = props.project;
 
-console.log(title, description, image, technologies);
+function getImageUrl(name: String) {
+  return new URL(`../assets/${name}`, import.meta.url).href;
+}
 </script>
 
 <template>
-  <main class="container">
-    {{ title }}
-    {{ description }}
-    {{ image }}
-    {{ technologies }}
+  <main
+    class="bg-red-500 container m-2 mx-2 flex h-96 w-96 flex-col justify-center rounded-sm"
+  >
+    <h2 class="self-center text-2xl">{{ title }}</h2>
+    <img
+      :src="getImageUrl(image)"
+      alt="project image"
+      class="aspect-square h-32 object-contain"
+    />
+    <div class="flex justify-center p-1">
+      <PillButton
+        v-for="technology in technologies"
+        :key="technology"
+        :icon="technology"
+        styles="bg-white rounded-full mx-2"
+      />
+    </div>
+    <p class="text-center">{{ description }}</p>
+    <ul class="flex justify-center">
+      <PillButton
+        v-for="link in links"
+        :key="link.url"
+        :url="link.url"
+        :icon="link.icon"
+        styles="bg-black rounded-full mx-1"
+      />
+    </ul>
   </main>
 </template>
