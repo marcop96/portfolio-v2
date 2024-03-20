43574d0b9b783e95bebdbe84a923eb989ca96359
“Marco <marco.pianaroli96@gmail.com>
2024-03-20 20:42:09 +0100

    refactor: swaps card

--- src/components/TheProjects.vue
+++ src/components/TheProjects.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import CardComponent from "./CardComponent.vue";
+import CardComponent2 from "./CardComponent2.vue";
 import { projects } from "../consts/Projects";
 </script>
 
 <template>
   <ul class="flex flex-col justify-center lg:flex-row lg:justify-center">
     <li class="mx-2" v-for="project in projects" :key="project.title">
-      <CardComponent :project="project" class="h-80 w-96 rounded-sm" />
+      <!-- <CardComponent :project="project" class="h-80 w-96 rounded-sm" /> -->
+      <CardComponent2 :project="project" class="my-2 h-80 w-96 rounded-sm" />
     </li>
   </ul>
 </template>
