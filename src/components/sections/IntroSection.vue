fd19856cc69046394b1d948f23331aaf04c43de3
“Marco <marco.pianaroli96@gmail.com>
2024-03-26 12:37:55 +0100

    style: adds animations and changes colors

--- src/components/sections/IntroSection.vue
+++ src/components/sections/IntroSection.vue
@@ -4,36 +4,40 @@ import PillButton from "../PillButton.vue";
 </script>
 
 <template>
-  <div class="container flex max-w-4xl flex-col justify-center text-center">
+  <div
+    class="container flex max-w-4xl flex-col justify-center text-center text-[#495057]"
+  >
     <img
       src="../../assets/marcoprofile.jpg"
       class="mx-2 h-48 w-48 self-center rounded-full object-cover"
     />
     <span class="text-center text-4xl"
       >{{ introText.title }} {{ introText.name }} <br />
-      <span class="text-red-500">{{ introText.role }}</span> developer</span
+      <span class="text-[#15aabf]">{{ introText.role }}</span> developer</span
     >
     <p class="text-2xl">{{ introText.description }}</p>
     <nav class="my-6 flex w-full justify-center">
       <PillButton
         url="https://www.linkedin.com/in/marco-pianaroli96/"
         icon="carbon:logo-linkedin"
-        styles=" w- bg-black rounded-full mx-1"
+        styles=" w- bg-black rounded-full mx-1 hover:bg-[#fab005] transition-all duration-300 ease-in-out"
       />
 
       <PillButton
         url="https://github.com/marcop96"
         icon="carbon:logo-github"
-        styles="  bg-black rounded-full mx-1"
+        styles="  bg-black rounded-full mx-1 hover:bg-[#fab005] transition-all duration-300 ease-in-out"
       />
       <PillButton
         url="mailto:pianarolimarco@gmail.com?subject=Front end developer position"
         icon="carbon:email"
-        styles="  bg-black rounded-full mx-1"
+        styles="  bg-black rounded-full mx-1 hover:bg-[#fab005] transition-all duration-300 ease-in-out"
       />
     </nav>
-    <RouterLink to="/resume">
-      <button class="rounded-full bg-black p-3 text-white hover:opacity-80">
+    <RouterLink to="/resume" target="_blank">
+      <button
+        class="rounded-full bg-black p-3 text-white transition-all duration-300 ease-in-out hover:bg-[#fab005] hover:opacity-80"
+      >
         view my resume
       </button>
     </RouterLink>
