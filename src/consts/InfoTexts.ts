7b54ed5d617f4815908bdd1fad53a581f8e5798d
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-08-12 09:07:21 +0200

    refactor: Update InfoTexts.ts to improve readability and clarity

--- src/consts/InfoTexts.ts
+++ src/consts/InfoTexts.ts
@@ -1,12 +1,12 @@
 export const aboutMe = {
   paragraph1:
-    "As a front-end developer, I combine my passion for gaming with my expertise in web development to create immersive and interactive experiences.",
+    "As a front-end developer with a deep passion for gaming, I blend creativity with technical expertise to craft immersive, interactive web experiences.",
   paragraph2:
-    "With VueJS as my weapon of choice, I craft responsive and dynamic web applications that captivate users and solve real-world problems.",
+    "Leveraging Nuxt as my go-to framework, I build responsive, dynamic web applications that engage users and address real-world challenges.",
   paragraph3:
-    "But I'm not just a developer. I'm also a sports enthusiast, always striving for teamwork, discipline, and perseverance—qualities that I bring to every project I undertake.",
+    "Beyond development, I'm a sports enthusiast who values teamwork, discipline, and perseverance—qualities I consistently bring to every project.",
   paragraph4:
-    "Join me on this exciting journey as we explore the vast landscape of front-end development and make a meaningful impact in the digital world.",
+    "Join me on this exciting journey as we explore the evolving landscape of front-end development and create meaningful impacts in the digital world.",
 };
 
 export const introText = {
@@ -14,11 +14,11 @@ export const introText = {
   name: "Marco Pianaroli",
   role: "Frontend",
   description:
-    "With a passion for creating beautiful and functional websites. I'm a hard worker and a quick learner, and I'm always looking for new opportunities to learn and grow as a developer.",
+    "With a strong passion for designing and developing beautiful, functional websites, I'm dedicated, quick to adapt, and always eager to expand my skills. I'm constantly on the lookout for opportunities to grow as a developer.",
   image: "../assets/marcoprofile.jpg",
   links: [
     {
-      name: "Github",
+      name: "GitHub",
       url: "https://github.com/marcop96",
       icon: "carbon:logo-github",
     },
@@ -29,21 +29,22 @@ export const introText = {
     },
   ],
 };
+
 export const contactMeText = {
   introduction:
-    "I'm always open to discussing web development work or partnership opportunities. If you have a project that you want to get started, think you need my help with something, or just fancy saying hey, then get in touch.",
+    "I'm always open to discussing web development work and partnership opportunities. If you're looking to start a new project, need my expertise, or simply want to connect, feel free to reach out.",
   buildTogether:
-    "Feel free to reach out if you're looking for a developer, have a question, or just want to connect. I'd be more than happy to discuss your project ideas or provide information about my skills and experience.",
+    "Whether you're seeking a developer, have questions, or just want to discuss ideas, I'd love to connect. I'm happy to share insights into my skills and experience or explore how we can collaborate.",
   email: "pianarolimarco@gmail.com",
   linkedIn: "https://www.linkedin.com/in/marco-pianaroli96/",
   gitHub: "https://www.github.com/marcop96",
-  availability: "open for opportunities",
+  availability: "Currently open to new opportunities",
 };
 
 export const whatAmIDoingNow = {
   currentProject:
-    "Currently I'm working on an incremental game using Nuxt. Also constantly going back to the current projects to improve them, since I'm always learning new stuff to stay updated with the current technologies",
+    "I'm currently developing an incremental game using Nuxt, while continuously refining my existing projects to incorporate the latest technologies and best practices.",
   currentPersonal:
-    "After a long time without any race, I'm back to training for a 15K",
-  currentLearning: "Currently learning about web performance and SEO",
+    "After a hiatus from racing, I'm back in training for a 15K run.",
+  currentLearning: "Currently enhancing my knowledge in web performance and SEO.",
 };
