569616e559df506baa9591dec7665c4f4ebe4a33
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-08-12 08:36:24 +0200

    refactor: @cardcomponent: user can click image to access project

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -16,11 +16,14 @@ function getImageUrl(name: string) {
     class="h-fit container m-4 p-4 flex flex-col justify-center rounded-xl shadow-lg bg-white bg-opacity-80"
   >
     <h2 class="self-center text-3xl text-gray-800 font-semibold mb-4">{{ title }}</h2>
-    <img
+
+      <a :href="links[1].url"
+      >
+      <img
       :src="getImageUrl(image)"
       alt="project image"
-      class="aspect-square h-40 object-cover rounded-lg shadow-md mb-4"
-    />
+      class="aspect-square h-40 object-cover rounded-lg w-full shadow-md mb-4"
+      /></a>
     <div class="flex justify-center mb-4">
       <div class=' flex flex-col justify-center mb-4' v-for="technology in technologies" :key="technology.name">
 
